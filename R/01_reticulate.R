9b898fa596c05a352771bdf2e5ec63bdf1769cbc
phineeeeeeeeeeeeeeeees <b04801015@g.ntu.edu.tw>
2021-01-16 22:13:45 +0100

    20210116 python configuration

--- R/01_reticulate.R
+++ R/01_reticulate.R
@@ -4,27 +4,74 @@
 # 2021-01-16
 #####################################################################################################
 
+library(dplyr)
+
+# =====================================
+# configure python
+# =====================================
+# set python to virtual environment
+Sys.setenv(RETICULATE_PYTHON = "Python/bin/python")
 library(reticulate)
-use_virtualenv("r-reticulate")
+
+#virtualenv_create("FlightStat")
+#use_virtualenv("FlightStat")
+
+# verify that you are using the python version you expect
+py_config()
+
+# =====================================
+# python dependencies
+# =====================================
+pd <- import("pandas")          # py_install("pandas")     # `import pandas as pd`
+
 
 # =====================================
 # test API "flightradar24"
 # =====================================
 # py_install("flightradar24" , pip = TRUE)                 # `pip install flightradar24`
-py_fr24 <- import("flightradar24")                         # `import flightradar24`
-py_fr24.API <- py_fr24$Api()                               # `fr = flightradar24.Api()`  
+fr24 <- import("flightradar24")                            # `import flightradar24 as fr24`
+fr24.API <- fr24$Api()                                     # `fr24.API = fr24.Api()`  
                                                            # 這裡要有括號才會初始化object 不然只是呼叫module而已
 
-# # Getting airports list                                  # an example
-airports <- py_fr24.API$get_airports()                     # 利用`$`來呼叫class中的instance function
+# Getting airports list                                    # an example
+airports <- fr24.API$get_airports()                        # 利用`$`來呼叫class中的instance function
 airports$rows[1]                                           # 可以看到第一個機場的資訊 python的dictionary到R中以list儲存
-airports_df <- data.frame(matrix(unlist(airports$rows) , nrow = length(airports$rows), byrow = TRUE))
-head(airports_df)                                          # 將list的儲存格式改為data frame
+airports_df <- pd$DataFrame$from_dict(airports$rows)       # 將list的儲存格式改為data frame
+
 
 # =====================================
 # test API "pyflightdata"
 # =====================================
 # py_install("pyflightdata" , pip = TRUE)
-py_FlightData <- import("pyflightdata")$FlightData()       # `from pyflightdata import FlightData`
+FlightData <- import("pyflightdata")$FlightData            # `from pyflightdata import FlightData`
 
-B18007 <- py_FlightData$get_history_by_tail_number(tail_number = "B-18007")
+# initiate class object FlightData; login to flightradar24 API
+FlightData.API <- FlightData()
+FlightData.API$login("crab6v8521@gmail.com" , "Phineas6629woho_")
+
+# Flight history by registration
+History_B18007 <- FlightData.API$get_history_by_tail_number(tail_number = "B-18007")
+str(History_B18007[[1]])                                   # 看看資料儲存的結構：list中還有list，identification; status; aircraft; owner; airline; airport; time
+History_B18918 <- FlightData.API$get_all_available_history_by_tail_number("B-18918")
+History_B18918[[145]]
+
+# Flight history by flight number
+History_CI61 <- FlightData.API$get_history_by_flight_number('CI61')
+
+# Information about the aircraft
+Info_PHBVA <- FlightData.API$get_info_by_tail_number("PH-BVA")
+
+# get countries
+pd$DataFrame$from_dict(FlightData.API$get_countries())
+
+# get airlines 
+airlines <- FlightData.API$get_airlines()
+airlines_df <- pd$DataFrame$from_dict(airlines)
+airlines_df[airlines_df$title == "China Airlines" , ]
+
+# get fleet (pass the airline-code from get_airlines)
+Fleet_CI <- FlightData.API$get_fleet("ci-cal")
+Fleet_CI_df <- pd$DataFrame$from_dict(Fleet_CI)
+
+# get airports
+FlightData.API$get_airports('Taiwan')
