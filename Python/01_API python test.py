d882f20965dded1519b3a4e6223e04a03eaca088
phineeeeeeeeeeeeeeeees <b04801015@g.ntu.edu.tw>
2021-01-16 22:14:08 +0100

    20210116 trying API functions

--- Python/01_API python test.py
+++ Python/01_API python test.py
@@ -40,9 +40,11 @@ f = FlightData()
 # This abstracts all the data access mechanism 
 # and also maintains the authenticated session to flightradar24 for users who have a paid membership.
 f.login("crab6v8521@gmail.com","Phineas6629woho_")
+f.logout()
 
 # Flight history by flight number
 f.get_history_by_flight_number('CI61')[-5:]
+f.get_flights_from_to(origin="FRA" , destination="TPE")
 
 # Flight history by registration
 History_B18918 = f.get_history_by_tail_number("B-18918")
@@ -62,10 +64,26 @@ f.get_countries()[:5]
 # get airlines
 airlines = f.get_airlines()
 airlines[0]["title"]
+
 # get airports
 f.get_airports('Taiwan')
+# get airport arrivals and departures
+TSA_arrivals = f.get_airport_arrivals("TSA")
+len(TSA_arrivals)
+TSA_departures = f.get_airport_departures("TSA")
+TSA_departures[0]
+# get airport onground
+f.get_airport_onground("TSA")
+# get airport details 
+f.get_airport_details("TSA")
+f.get_airport_metars("TSA")
+f.get_airport_metars_hist("TSA")
+f.get_airport_weather("TSA")
+
 
 # get fleet  (pass the airline-code from get_airlines)
-f.get_fleet('emirates-ek-uae')
+f.get_fleet('ci-cal')
+
+
 
 
